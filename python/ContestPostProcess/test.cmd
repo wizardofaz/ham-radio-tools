61277c4a569a24edc3d9abc512a2ebeee53db51b
Bill Bennett <bill@wizardofaz.net>
2026-03-12 12:37:59 -0700

    make sure test.cmd running in its own directory

--- python/ContestPostProcess/test.cmd
+++ python/ContestPostProcess/test.cmd
@@ -12,8 +12,12 @@ IF "%QRZ_PASSWORD%"=="" (
     set /p QRZ_PASSWORD=Enter QRZ password: 
 )
 
+REM make sure we're running from the right place
+pushd %~dp0
+
 copy testdata\input\qrz_cache.json testdata\output\
 python -m contest_summary "./testdata/input/sample_log.adi" --outdir ./testdata/output --qrz yes --map states_dx
 rem python -m contest_summary "\temp\test_split_by_operator\W1AW_7_2026-0225_to_2026-0303 (whole event).adi" --qrz yes --map na_states_dx
 
-type \temp\test_split_by_operator\summary.txt
+type testdata\output\summary.txt
+popd 
